7d941738353f62a8335ae87ac4f6108582d69675
Andry Nantenaina <65734754+andrynante261@users.noreply.github.com>
2024-03-15 15:42:49 +0300

    Update Banner.js

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import LeftBanner from './LeftBanner';
 import RightBanner from './RightBanner';
-import Particle from "../Particle";
 
 const Banner = () => {
   return (
-    <>
-      <Particle /> {/* Adding Particle component */}
       <section
         id="home"
         className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black" 
